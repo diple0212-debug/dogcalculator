cd37c7d65880370bf2c80e5a7cb738decffcfcef
diple0212-debug <diple0212@gmail.com>
2026-01-25 21:22:10 +0900

    버전 충돌 오류 해결 및 코드 안정화

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import AgeCalculator from './pages/AgeCalculator';
-import ObesityCalculator from './pages/ObesityCalculator';
-import FoodCalculator from './pages/FoodCalculator';
-import About from './pages/About';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
+import Layout from './components/Layout.tsx';
+import AgeCalculator from './pages/AgeCalculator.tsx';
+import ObesityCalculator from './pages/ObesityCalculator.tsx';
+import FoodCalculator from './pages/FoodCalculator.tsx';
+import HealthAdvisor from './pages/HealthAdvisor.tsx';
+import About from './pages/About.tsx';
+import Terms from './pages/Terms.tsx';
+import Privacy from './pages/Privacy.tsx';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<AgeCalculator />} />
           <Route path="/obesity" element={<ObesityCalculator />} />
           <Route path="/food" element={<FoodCalculator />} />
+          <Route path="/health-advisor" element={<HealthAdvisor />} />
           <Route path="/about" element={<About />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/privacy" element={<Privacy />} />
